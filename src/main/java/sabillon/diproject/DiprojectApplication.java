f8d06eae8c7b21a093c1ffb85eebd986afe6791f
Miguel Sabillon <mikesabillon2014@gmail.com>
2019-08-31 21:56:42 +0200

    first commit of base project

---
+++ src/main/java/sabillon/diproject/DiprojectApplication.java
@@ -0,0 +1,13 @@
+package sabillon.diproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiprojectApplication.class, args);
+	}
+
+}
