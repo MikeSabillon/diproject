fd51e0384a8fbf378a53f4c24ab5db19363080f4
Miguel Angel Sabillon <MikeSabillon@users.noreply.github.com>
2019-09-26 22:38:15 +0200

    Create travis yml file (#3)
    * Created component examples
    
    * Added travis yml

---
+++ src/main/java/sabillon/diproject/components/ByeComponent.java
@@ -0,0 +1,15 @@
+package sabillon.diproject.components;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ByeComponent {
+
+	public ByeComponent() {
+		log.info("Bye component initialized");
+	}
+
+}
